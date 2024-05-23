215523aa1c7e989fd0dc21b1298919de843ca5de
WillianKakihata <161545901+WillianKakihata@users.noreply.github.com>
2024-05-23 01:29:41 +0000

    dada

--- atividade.js
+++ atividade.js
@@ -1,1 +1,15 @@
-
+const map1 = new Map();
+const produto = [
+    {nome: "maca", categoria: "fruta"},
+    {nome: "banana", categoria: "fruta"},
+    {nome: "melao", categoria: "fruta"},
+    {nome: "manga", categoria: "fruta"},
+    {nome: "cachorro", categoria: "animal"},
+    {nome: "picanha", categoria: "carne"},
+    {nome: "fralda", categoria: "utensilios"}
+];
+for(const nome of produto) {
+    const categoria = produto.categoria;
+    if (map1.has(categoria))
+}
+console.log(map1)
